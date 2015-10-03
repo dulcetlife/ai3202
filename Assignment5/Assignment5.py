627c318541d779e85870af137c783d2a8517fa8c
Henrik Larsen <hela4200@colorado.edu>
2015-10-03 17:39:47 -0600

    Added World1.txt

--- Assignment5/Assignment5.py
+++ Assignment5/Assignment5.py
@@ -1,1 +1,17 @@
 #Assignment 5
+
+
+
+def inputAndRead():
+	if len(sys.argv) != 3:
+		print "Need to provide world and a value for ε!"
+		return(False, False)
+	elif sys.argv[1] != "World1.txt":
+		print "Incorrect world provided! Need to be World1.txt"
+		return(False, False)
+	elif sys.argv[2] != int 
+		print "Need to provide a value of ε!"
+		return(False, False)
+	else:
+		m = sys.argv[1]
+		return (m, sys.argv[2])
